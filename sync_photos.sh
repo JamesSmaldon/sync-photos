857d9925696934d901ab63baa9057fdf9d4b5522
James Smaldon <james.smaldon@gmail.com>
2017-11-04 11:04:36 +0000

    Fix spaces in path

--- sync_photos.sh
+++ sync_photos.sh
@@ -1,6 +1,6 @@
 SERVER_ADDR=server
 DEST_FOLDER=/media/md3/nas/media/iphoto
-SRC_FOLDER="/Users/vicki/Pictures/iPhoto Library.photolibrary"
+SRC_FOLDER="'/Users/vicki/Pictures/iPhoto Library.photolibrary'"
 USERNAME=vicki
 
 rsync -rltzD --progress ${SRC_FOLDER}/ ${USERNAME}@${SERVER_ADDR}:${DEST_FOLDER}
