07846563f1a3aa5a163a1d72af9ef79933c2cb54
James Smaldon <james.smaldon@gmail.com>
2017-11-04 11:02:13 +0000

    Add source folder.

--- sync_photos.sh
+++ sync_photos.sh
@@ -1,5 +1,6 @@
 SERVER_ADDR=server
 DEST_FOLDER=/media/md3/nas/media/iphoto
+SRC_FOLDER="/Users/vicki/Pictures/iPhoto Library.photolibrary"
 USERNAME=vicki
 
-rsync -rltzD --progress ./ ${USERNAME}@${SERVER_ADDR}:${DEST_FOLDER}
+rsync -rltzD --progress ${SRC_FOLDER}/ ${USERNAME}@${SERVER_ADDR}:${DEST_FOLDER}
